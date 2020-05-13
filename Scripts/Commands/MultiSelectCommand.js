62b25cecf07171f16b2cca75a79c238e568909a1
Vanderlei Arruda <vanderlei.arruda.jr@gmail.com>
2020-05-13 14:08:17 -0300

    add

---
+++ Scripts/Commands/MultiSelectCommand.js
@@ -0,0 +1,114 @@
+﻿function MultiSelectCommand(controller) {
+    ICommand.call(this);
+
+    this.responseText = [];
+
+    this.controller = controller;
+
+    //Gets the language
+    this.userLang = navigator.language || navigator.userLanguage || navigator.languages; //controller.chatter.userLanguage;
+
+    if (this.userLang != "zh-CHS")
+        this.userLang = this.userLang.split("-")[0];    
+}
+
+//Herda a classe abstrata ICommand
+MultiSelectCommand.prototype = new ICommand();
+
+//Acerta o construtor pois agora está apontando para ICommand
+MultiSelectCommand.prototype.constructor = MultiSelectCommand;
+
+MultiSelectCommand.prototype.execute = function () {
+    this.originalResponseText = controller.chatter.botText;
+    var result = this.originalResponseText;
+
+    var element = result;
+    var htmlOptions = '<br />';
+    var valueSophieMessage = this.getParameter('values', element);
+    var answers = valueSophieMessage.toString();
+    answers = answers.split(";");
+
+    /// Remove if exist class
+    $("input").removeClass("OptionsTag");
+
+    for (var i = 0; i < answers.length; i++) {
+        var text = answers[i].trim();
+        htmlOptions += 
+                '<div class="checkbox">'+
+                    '<label class="OptionsLabelTag">'+
+                        '<input class="OptionsTag" onclick = "MultiSelect()" type = "checkbox" value = "' + text + '" >'+
+                            '<span class="ckboxLabel">&ensp;' + text + '</span>'+
+                        '</label>'+
+                '</div>';
+    }
+
+    htmlOptions += '<div><input class="btn btn-info mr10 mb10 btnQuestionSubmit" type="submit" onclick="sendQuestions(); return false;" value="' + translation[this.userLang]['sendQuestions'] + '">';
+    htmlOptions += '<input class="btn btn-danger mr10 mb10 btnQuestionCancel" type="submit" onclick="cancelQuestions(); return false;" value="' + translation[this.userLang]['cancel'] + '"></div>';
+
+    result = result.replace("[c:multiselect values=" + valueSophieMessage + "]", htmlOptions);
+
+    /// Block textArea and send button
+    eventDisableComponents(true);
+
+    this.controller.chatter.botText = result;
+};
+
+MultiSelectCommand.prototype.getParameter = function (name, responseText) {
+    var regex = new RegExp(name + "=([^#=]*)[=|\\]]"), results = regex.exec(responseText);
+    return results === null ? "" : results[1].replace(/label|url|target|value/g, '');
+};
+
+MultiSelectCommand.prototype.isMyCommand = function () {
+    this.originalResponseText = this.controller.chatter.botText;
+    var regex = /(\[c\:multiselect\]*)+(\s*\[[^\[\]]*\]\s*)*([^\[\]]*\])*/gi;
+    var results = [], result;
+    while ((result = regex.exec(this.originalResponseText)) !== null) {
+        var text = '';
+        if (result != null && result.length > 1) {
+            for (var i = 1; i < result.length; i++)
+                if (result[i] != undefined)
+                    text += result[i];
+            results.push(text);
+        }
+    }
+    this.executeCommand = results.length > 0;
+    if (this.executeCommand)
+        this.responseText = results[0];
+    return this.executeCommand;
+}
+
+function sendQuestions() {
+
+    disabelMultiSelect();
+    eventDisableComponents(false);
+
+    var selected = [];
+    $.each($(".OptionsTag:checked"), function () {
+        selected.push($(this).val());
+    });
+
+    var sendQuestions = selected.join(';');
+    
+    this.controller.sendMessage(sendQuestions, sendQuestions, false);
+    return;
+}
+
+function cancelQuestions() {
+   
+    disabelMultiSelect();
+    eventDisableComponents(false);
+
+    this.controller.sendMessage('escapeflow', 'escapeflow', false);
+    return;
+}
+
+function disabelMultiSelect() {
+    $("input.btnQuestionSubmit").prop("disabled", true);
+    $("input.btnQuestionCancel").prop("disabled", true);
+    $("div.checkbox .OptionsTag").attr('disabled', true);
+}
+function eventDisableComponents(status) {
+    /// Block textArea and send button
+    $('#btnSend').prop("disabled", status);
+    $("#txtQuestion").prop("disabled", status);
+}
---
+++ Scripts/Controllers/ChatterController.js
@@ -0,0 +1,170 @@
+﻿function ChatterController(chatter, printer) {
+    this.chatter = chatter;
+    this.printer = printer;
+    this.currentLanguage = navigator.language || navigator.userLanguage || navigator.languages;//chatter.userLanguage;
+    this.liveAgent = false;
+    this.systemName = null;
+
+    if (this.currentLanguage != "zh-CHS")
+        this.currentLanguage = this.currentLanguage.split("-")[0]; 
+
+}
+
+ChatterController.prototype.sendMessage = function (label, question, display, language, showAnswer = true) {
+
+    if (question != '') {
+        //$('#txtQuestion').prop("disabled", true);
+        if (display) {
+            this.printer.createDivMessage(this.chatter.systemName, this.chatter.userInfo.userName, label);
+        }
+
+        if (!this.liveAgent && showAnswer)
+            this.statusConversation("writing");
+            
+        this.chatter.userText = question;
+        this.chatter.botText = "";
+        this.chatter.userLanguage = language;
+
+        var formData = new FormData();
+
+        if (showAnswer) {
+            $.ajax({
+                url: rootDir + 'GetConversation',
+                cache: false,
+                data: this.chatter,
+                method: "POST",
+                context: this,
+                //success: function () {
+                //    clearInterval(intervalId);
+                //    intervalId = setInterval(receiveTimeout, 500);
+                //},
+                error: function (error) {
+                    this.statusConversation("offline");
+                    console.log("Error getting answer of the question. Error: " + error);
+                }
+            });
+        }
+        else {
+            $.ajax({
+                url: rootDir + 'Respond',
+                cache: false,
+                contentType: "application/json; charset=utf-8",
+                data: `{"sessionID":"${this.chatter.sessionId}","userInput":"${question}","language":"${language}"}`,
+                datatype: "json",
+                method: "POST",
+                error: function (error) {
+                    this.statusConversation("offline");
+                    console.log("Error getting answer of the question. Error: " + error);
+                }
+            });
+        }
+        if ($("#LiveAgentBtnStart")) {
+            if (this.liveAgent)
+                showEndBtn();
+            else
+                showStartBtn();
+        }
+    }
+};
+
+ChatterController.prototype.receiveMessage = function (data) {
+
+    this.chatter.botText = data.botText.replace("[c:skypestarted]","");
+    this.chatter.userLanguage = data.userLanguage;
+    this.liveAgent = (/true/i).test(data.liveAgent);
+    this.pw = false;
+
+    // Check if talk with live Agent
+    if (data.agentName && !this.systemName) {
+        this.systemName = this.chatter.systemName;
+        this.chatter.systemName = data.agentName;
+    }
+    else if (!data.agentName && this.systemName) {
+        this.chatter.systemName = this.systemName;
+        this.systemName = null;
+    }
+
+    if (controller.chatter.botText != '') {
+
+        //text = this.chatter.botText;
+        var commands = [];
+
+        commands.push(new PhoneCommand(controller));
+        commands.push(new LinkCommand(this));
+        commands.push(new NewLineCommand(this));
+        commands.push(new ImgCommand(this));
+        commands.push(new PasswordCommand(this));
+        commands.push(new UploadCommand(this));
+        commands.push(new MultiSelectCommand(controller));
+
+        var poscommands = [];
+
+        //comandos antes da resposta
+        for (var i = 0; i < commands.length; i++)
+            if (commands[i].isMyCommand())
+                commands[i].execute();
+    }
+
+    this.chatter.botText.split("[c:newdialog]").forEach(function (item) {
+
+        if (item.indexOf("[@user@]") > 0) {
+            if (item.indexOf("[@user@]startup") == -1) {
+                var userResp = item.replace("[@user@]", "");
+                controller.printer.createDivMessage(controller.chatter.systemName, "user", userResp, controller.pw);
+            }
+        } else {
+            if (item.trim() != "")
+                controller.printer.createDivMessage(controller.chatter.systemName, "bot", item, controller.pw);
+        }
+    });
+
+    if (this.chatter.userLanguage !== this.currentLanguage) {
+        this.refreshLanguage();
+    }
+
+    this.statusConversation("online");
+    checkForAgent();
+}
+
+ChatterController.prototype.statusConversation = function (text) {
+    if (text == "writing") {
+        this.printer.createWritingDiv();
+        this.updateStatusView(this.chatter.systemName + translation[this.currentLanguage]['isWriting']);        
+    }
+    else if (text == "online") {
+        this.printer.removeWritingDiv();
+        this.updateStatusView(translation[this.currentLanguage]['pleaseType']);        
+    }
+    else if (text == "sending") {
+        this.printer.removeWritingDiv();
+        this.updateStatusView(translation[this.currentLanguage]['sending']);
+    }
+    else {
+        this.printer.removeWritingDiv();
+        this.updateStatusView(translation[this.currentLanguage]['isOffline']);
+    }
+}
+
+ChatterController.prototype.updateStatusView = function (status) {
+    $('div[data-chatter="bot-status"]').text(status);
+}
+
+ChatterController.prototype.timeoutCall = function () {
+    this.sendMessage('timeoutcall', 'timeoutcall', false);
+}
+
+ChatterController.prototype.refreshLanguage = function () {
+    //this.currentLanguage = this.chatter.userLanguage;
+    //this.currentLanguage = this.userLang;
+
+    if (this.currentLanguage in translation) {
+        $('#headerTitle').text(translation[this.currentLanguage]['title']);
+        $('#btnSend').text(translation[this.currentLanguage]['send']);
+        $('#txtQuestion').attr("placeholder", translation[this.currentLanguage]['questionPlaceHolder']);
+        $('#h6Logoff').html(translation[this.currentLanguage]['logoff']);
+        $('#h6TitleChatter').html(translation[this.currentLanguage]['title']);
+    }
+    else
+        this.currentLanguage = 'en';
+
+}
